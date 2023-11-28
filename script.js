bc5b8272a5914ba294ed9887233fb191340459c7
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-11-28 23:06:42 +0530

    drum kit

--- script.js
+++ script.js
@@ -10,19 +10,19 @@ kits.forEach((kit) => {
     containerEl.appendChild(btnEl);
 
     const audioEl = document.createElement("audio");
-    audioEl.src = "sounds/ "+ kit + ".mp3";
+    audioEl.src = "sounds/" + kit + ".mp3";
     containerEl.appendChild(audioEl);
-    btnEl.addEventListener("click",() => {
-        audioEl.play();
+    btnEl.addEventListener("click", () => {
+      audioEl.play();
     });
-
+    
     window.addEventListener("keydown", (event) => {
-        if (event.key === kit.slice(0,1)) {
-            audioEl.play();
-            btnEl.style.transform = "scale(.9)";
-            setTimeout(() => {
-                btnEl.style.transform = "scale(1)";
-            }, 100)
-        }
+      if (event.key === kit.slice(0, 1)) {
+        audioEl.play();
+        btnEl.style.transform = "scale(.9)";
+        setTimeout(() => {
+          btnEl.style.transform = "scale(1)";
+        }, 100);
+      }
     });
-});
+  });
